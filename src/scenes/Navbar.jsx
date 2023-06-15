fb4f97796bc5a253f6c0f27d4cd494434d49b4ea
Ramaj Johnson <rjohnson3795@gmail.com>
2023-06-15 12:36:39 -0500

    Navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -4,12 +4,24 @@ import AnchorLink from "react-anchor-link-smooth-scroll/lib/anchor-link";
 const Navbar = ({selectedPage, setSelectedPage}) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false)
 
-    const isMdScreen = useMediaQuery("(min-width: 769px)")
+    const aboveSmScreen = useMediaQuery("(min-width: 769px)")
 
   return (
       <nav className={`z-40 w-full fixed top-0 py-6`}>
-        <div className='flex items-center justify-between mx-auto w-5/6'>
-           <h4 className="font-playfair"></h4>
+        <div className="flex items-center justify-between mx-auto w-5/6">
+           <h4 className="font-playfair text-3xl font-bold">
+               RJ
+           </h4>
+
+{/*Desktop Nav*/}
+           {aboveSmScreen ?
+               (
+
+               ) :
+               (
+
+               )
+           }
         </div>
       </nav>
   )
