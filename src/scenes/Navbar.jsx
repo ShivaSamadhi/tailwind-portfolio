1913ce8e0300e644243b2b0c9708cd10f6e94603
Ramaj Johnson <rjohnson3795@gmail.com>
2023-06-15 12:52:21 -0500

    Navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,6 +1,20 @@
 import {useState} from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
 import AnchorLink from "react-anchor-link-smooth-scroll/lib/anchor-link";
+
+const Link = ({page, selectedPage, setSelectedPage}) => {
+    const pageLowerCase = page.toLowerCase()
+
+    return (
+        <AnchorLink
+            className={`${selectedPage === pageLowerCase ? "text-yellow": ""} hover:text-yellow transition duration-500`}
+            href={`#${pageLowerCase}`}
+            onClick={()=> setSelectedPage(pageLowerCase)}
+        >
+            {page}
+        </AnchorLink>
+    )
+}
 const Navbar = ({selectedPage, setSelectedPage}) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false)
 
@@ -16,10 +30,17 @@ const Navbar = ({selectedPage, setSelectedPage}) => {
 {/*Desktop Nav*/}
            {aboveSmScreen ?
                (
-
+                   <div className="flex justify-between gap-16 text-sm font-semibold">
+                       <Link
+                           page="Home"
+                           selectedPage={selectedPage}
+                           setSelectedPage={setSelectedPage}/>
+                   </div>
                ) :
                (
+                   <div>
 
+                   </div>
                )
            }
         </div>
