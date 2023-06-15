d7eb72d1531205631c95aca2af2d07525f16d4b6
Ramaj Johnson <rjohnson3795@gmail.com>
2023-06-15 12:20:53 -0500

    Navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,9 +1,17 @@
-
+import {useState} from "react";
+import useMediaQuery from "../hooks/useMediaQuery";
+import AnchorLink from "react-anchor-link-smooth-scroll/lib/anchor-link";
 const Navbar = ({selectedPage, setSelectedPage}) => {
-  return (
-      <div>
+    const [isMenuToggled, setIsMenuToggled] = useState(false)
 
-      </div>
+    const isMdScreen = useMediaQuery("(min-width: 769px)")
+
+  return (
+      <nav className={`z-40 w-full fixed top-0 py-6`}>
+        <div className='flex items-center justify-between mx-auto w-5/6'>
+           <h4 className="font-playfair"></h4>
+        </div>
+      </nav>
   )
 }
 
