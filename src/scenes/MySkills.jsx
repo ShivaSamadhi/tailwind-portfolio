d89311eb302dc9257fac83a434eb5b93abcdfb8c
Ramaj Johnson <rjohnson3795@gmail.com>
2023-06-23 10:52:24 -0500

    Skills Section

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -14,7 +14,7 @@ const MySkills = () => {
 			  className="md:flex md:justify-between md:gap-16 mt-32"
 		  >
 			  {isLgScreen ? (
-				  <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+				  <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:w-full before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]">
 					  <img
 						  alt="profile"
 						  className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
@@ -24,7 +24,7 @@ const MySkills = () => {
 			  ) : (
 				  <img
 					  alt="profile"
-					  className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full mx-auto max-w-[800px] md:max-w-[600px]"
+					  className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full mx-auto max-w-[600px] md:max-w-[600px]"
 					  src="../assets/skills-image.png"
 				  />
 			  )}
